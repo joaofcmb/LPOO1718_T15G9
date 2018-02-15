5cb558c85be834659eaa9e5ee2d3e06d67012384
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2018-02-15 22:05:27 +0000

    Added Guard Patrol to the Starting Map

--- LPOO_Guided_Project/src/DungeonKeep.java
+++ LPOO_Guided_Project/src/DungeonKeep.java
@@ -4,15 +4,16 @@ public class DungeonKeep {
 	public static void main(String[] args) {
 		Scanner playerInput = new Scanner(System.in);
 
-		
-
 		int[] playerPos = {1,1};
 		int[] nextplayerPos = new int[2];
 
-
 		while(true) {
+			// Render map
 			Map.toString(Map.startingMap);
+			
+			// check if guard moved near player
 
+			// calculate new player position
 			nextplayerPos = playerPos.clone();
 
 			char keyInput = playerInput.next().charAt(0);
@@ -35,6 +36,7 @@ public class DungeonKeep {
 				return;
 			}
 
+			// check for map collisions and triggers caused by new player position
 			switch(Map.startingMap[nextplayerPos[0]][nextplayerPos[1]]) {
 			case 'k':
 				Map.startingMap[5][0] = Map.startingMap[6][0] = 'S';
@@ -51,6 +53,13 @@ public class DungeonKeep {
 				return;
 			}
 			}
+			
+			// check if player moved near guard
+			
+			// update guard's position and change map accordingly (Notice it's duplicate code from player)
+			Map.startingMap[Guard.guardPos[0]][Guard.guardPos[1]] = ' ';
+			Guard.updatePos();
+			Map.startingMap[Guard.guardPos[0]][Guard.guardPos[1]] = 'G';
 		}		
 	}
 }
--- LPOO_Guided_Project/src/Guard.java
+++ LPOO_Guided_Project/src/Guard.java
@@ -1,21 +1,49 @@
 import java.util.Arrays;
 
 public class Guard {
-	public enum Direction{ UP, DOWN, LEFT, RIGHT }
+	public enum Direction{ NONE, UP, DOWN, LEFT, RIGHT } // Temporary till a uniform class is developed for handling positions
 
 	public static Direction guardDirection = Direction.LEFT;
 	static int[] guardPos = {1,8};
 
-	public static void move() {
+	// FUNCTION: updates guard's position based on its patrol (Doesn't render new position)
+	public static void updatePos() {
+		// check if there is movement direction change for the guard patrol
 		switch(Arrays.toString(guardPos)) {
-		case "[1][8]":
-		case "[5][7]":guardDirection = Direction.LEFT;
-
-		case "[1][7]":
-		case "[5][1]":guardDirection = Direction.DOWN;
-
-		case "[6][1]":guardDirection = Direction.RIGHT;
-		case "[6][8]":guardDirection = Direction.UP;
+		case "[1, 8]":
+		case "[5, 7]":
+			guardDirection = Direction.LEFT;
+			break;
+		case "[1, 7]":
+		case "[5, 1]":
+			guardDirection = Direction.DOWN;
+			break;
+		case "[6, 1]":
+			guardDirection = Direction.RIGHT;
+			break;
+		case "[6, 8]":
+			guardDirection = Direction.UP;
+			break;
+		default:
+			break;
 		}
+
+		// update guard's position in the matrix
+		switch(guardDirection) {
+		case UP:		
+			guardPos[0]--;
+			break;
+		case DOWN: 		
+			guardPos[0]++;
+			break;
+		case LEFT:		
+			guardPos[1]--;
+			break;
+		case RIGHT:		
+			guardPos[1]++;
+			break;
+		default:
+			break;
+		}			
 	}
 }
