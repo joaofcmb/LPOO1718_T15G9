42db5bd91d269c30db73c2506f51b676e3020c54
joaofcmb <joaofernandomeireles@gmail.com>
2018-03-02 14:02:53 +0000

    Started working on re-implementing Patrols

--- LPOO_Guided_Project/src/dkeep/logic/Patrol.java
+++ LPOO_Guided_Project/src/dkeep/logic/Patrol.java
@@ -1,29 +1,57 @@
 package dkeep.logic;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Patrol {
-	private List<Integer[]> nodes;
+	private List<Integer[]> nodes; // (x, y)
 	private List<Game.Direction> direction;
 	
 	
-	Patrol(Integer[][] nodes, Game.Direction[] dir) {
+	/*
+	 * Patrol syntax: "x, y, Dir,..." // each 3 elems is a node, must be ordered (to infer the suspicion direction too)
+	 * Spacing of elements must be exactly ", ".  Dir = U, L, D, R (initials of direction)
+	 */
+	
+	Patrol(String config) {
 		// Add the nodes and dir to the object
-		this.nodes = new ArrayList<Integer[]>();
-		for (Integer[] node : nodes) {
-			this.nodes.add(node);
+		Scanner patrolConfig = new Scanner(config);
+		
+		patrolConfig.useDelimiter(", ");
+		
+		while (patrolConfig.hasNextInt()) {
+			int x = patrolConfig.nextInt();
+			int y = patrolConfig.nextInt();
+			Game.Direction direction = directionConfig(patrolConfig.next());
+			
+			// add to ArrayLists
 		}
 		
-		direction = new ArrayList<Game.Direction>(Arrays.asList(dir));
+		patrolConfig.close();
+	}
+	
+	private Game.Direction directionConfig(String s) {
+		switch(s.charAt(0)) {
+		case 'U':
+			return Game.Direction.UP;
+		case 'L':
+			return Game.Direction.LEFT;
+		case 'D':
+			return Game.Direction.DOWN;
+		case 'R':
+			return Game.Direction.RIGHT;
+		default:
+			return Game.Direction.NONE;	
+		}
 	}
 	
 	public Game.Direction nodeDirection(int x, int y) {
 		for(Integer[] node: nodes) {
 			if (node[0] == x && node[1] == y) {
 				// Found a node, return new direction
-				return direction.get(nodes.indexOf(node));
+				return directions.get(nodes.indexOf(node));
 			}
 		}
 		
