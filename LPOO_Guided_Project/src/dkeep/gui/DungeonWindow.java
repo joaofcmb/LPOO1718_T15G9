3ce4e59e0c1dd3ba9d39806f2f63c73fb40a8a70
Diogo Teixeira <diogo_alex@hotmail.com>
2018-03-20 15:06:53 +0000

    DungeonWindow from last class
    Having some problems with git

--- LPOO_Guided_Project/src/dkeep/gui/DungeonWindow.java
+++ LPOO_Guided_Project/src/dkeep/gui/DungeonWindow.java
@@ -8,22 +8,25 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import java.awt.Font;
-import java.awt.event.*;
+import javax.swing.SwingConstants;
+
+import dkeep.logic.Game;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class DungeonWindow {
-
+	private Game game = new Game();
 	private JFrame frame;
-	private JTextField ogreTxt;
-	JLabel lblGamestatus;
+	private JTextField ogreTextField;
+	private JLabel gameStatuslbl;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		Game game = new Game(1);
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -48,157 +51,190 @@ public class DungeonWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 629, 540);
+		frame.setBounds(100, 100, 591, 495);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JLabel ogreLabel = new JLabel("Number of Ogres");
-		ogreLabel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		ogreLabel.setBounds(10, 10, 128, 27);
-		ogreLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		ogreLabel.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		ogreLabel.setBounds(328, 10, 98, 22);
+
+		ogreTextField = new JTextField();
+		ogreTextField.setBounds(436, 12, 130, 19);
+		ogreTextField.setColumns(10);
+
+		JLabel guardLabel = new JLabel("Guard Personallity");
+		guardLabel.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		guardLabel.setBounds(328, 41, 98, 22);
+
+		JComboBox<String> guardComboBox = new JComboBox<String>();
+		guardComboBox.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		guardComboBox.addItem("Rookie");
+		guardComboBox.addItem("Suspicious");
+		guardComboBox.addItem("Drunk");
+		guardComboBox.setBounds(436, 41, 130, 22);
+
+		JButton btnStart = new JButton("Start Game");
+		btnStart.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnStart.setBounds(302, 406, 96, 41);
+
+		JButton btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnExit.setBounds(10, 406, 108, 41);
+
+		JTextArea gameArea = new JTextArea();
+		gameArea.setFont(new Font("Courier New", Font.PLAIN, 13));
+		gameArea.setEditable(false);
+		gameArea.setBounds(10, 87, 327, 302);
+
+		JButton btnLeft = new JButton("Left");
+		btnLeft.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnLeft.setEnabled(false);
+		btnLeft.setBounds(347, 202, 98, 47);
+
+		JButton btnRight = new JButton("Right");
+		btnRight.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnRight.setEnabled(false);
+		btnRight.setBounds(468, 202, 98, 47);
+
+		JButton btnDown = new JButton("Down");
+		btnDown.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnDown.setEnabled(false);
+		btnDown.setBounds(407, 259, 98, 47);
+
+		JButton btnUp = new JButton("Up");
+		btnUp.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnUp.setEnabled(false);
+		btnUp.setBounds(407, 145, 98, 47);
+
+		gameStatuslbl = new JLabel("Welcome to Dungeon Keep");
+		gameStatuslbl.setHorizontalAlignment(SwingConstants.CENTER);
+		gameStatuslbl.setToolTipText("Nothing to see here");
+		gameStatuslbl.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		gameStatuslbl.setBounds(10, 15, 308, 65);
+
+		btnExit.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.exit(0);
+					}
+				});
+		/*
+		 * START BUTTON
+		 */
+		btnStart.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int n = 0;
+						try {
+							n = Integer.parseUnsignedInt(ogreTextField.getText());
+						}
+						catch(NumberFormatException ex) {
+							JOptionPane.showMessageDialog(ogreTextField, "Introduza um inteiro positivo");
+							return;
+						}
+						if(n == 0) {
+							JOptionPane.showMessageDialog(ogreTextField, "Introduza um inteiro positivo.");
+							return;
+						}
+						if(n >= 2) { //FOR TESTING
+							JOptionPane.showMessageDialog(ogreTextField, "Use this to choose the level for now");
+							return;
+						}
+
+						btnUp.setEnabled(true);
+						btnLeft.setEnabled(true);
+						btnRight.setEnabled(true);
+						btnDown.setEnabled(true);
+						btnStart.setEnabled(false);
+						game = new Game();
+						gameArea.setText(game.toString());
+						gameStatuslbl.setText("You must escape!!. Good Luck!");
+					}
+				});
+
+		/*
+		 * DIRECTIONAL BUTTONS
+		 */
+		
+		btnUp.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						game.update(Game.Direction.UP);
+						gameArea.setText(game.toString());
+						if(game.gameLost() || game.gameWon()) {
+							btnUp.setEnabled(false);
+							btnLeft.setEnabled(false);
+							btnRight.setEnabled(false);
+							btnDown.setEnabled(false);
+							gameStatuslbl.setText("Game Over");
+						}
+					}
+				});
+
+		btnDown.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						game.update(Game.Direction.DOWN);
+						gameArea.setText(game.toString());
+						if(game.gameLost() || game.gameWon()) {
+							btnUp.setEnabled(false);
+							btnLeft.setEnabled(false);
+							btnRight.setEnabled(false);
+							btnDown.setEnabled(false);
+							gameStatuslbl.setText("Game Over");
+						}
+					}
+				});
+
+		btnLeft.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						game.update(Game.Direction.LEFT);
+						gameArea.setText(game.toString());
+						if(game.gameLost() || game.gameWon()) {
+							btnUp.setEnabled(false);
+							btnLeft.setEnabled(false);
+							btnRight.setEnabled(false);
+							btnDown.setEnabled(false);
+							gameStatuslbl.setText("Game Over");
+						}
+					}
+				});
+
+		btnRight.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						game.update(Game.Direction.RIGHT);
+						gameArea.setText(game.toString());
+						if(game.gameLost() || game.gameWon()) {
+							btnUp.setEnabled(false);
+							btnLeft.setEnabled(false);
+							btnRight.setEnabled(false);
+							btnDown.setEnabled(false);
+							gameStatuslbl.setText("Game Over");
+						}
+					}
+				});
+
+		/*
+		 * ADD TO CONTENT PANE
+		 */
 		frame.getContentPane().add(ogreLabel);
-
-		JLabel guardLabel = new JLabel("Guard Personality");
-		guardLabel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		guardLabel.setBounds(10, 52,  128, 27);
+		frame.getContentPane().add(ogreTextField);
 		frame.getContentPane().add(guardLabel);
+		frame.getContentPane().add(guardComboBox);
+		frame.getContentPane().add(btnStart);
+		frame.getContentPane().add(btnExit);
+		frame.getContentPane().add(gameArea);
+		frame.getContentPane().add(btnLeft);
+		frame.getContentPane().add(btnRight);
+		frame.getContentPane().add(btnDown);
+		frame.getContentPane().add(btnUp);
+		frame.getContentPane().add(gameStatuslbl);
 
 
-		JComboBox<String> guardCombo = new JComboBox<String>();
-		guardCombo.addItem("Rookie");
-		guardCombo.addItem("Drunkard");
-		guardCombo.addItem("Suspicious");
-		guardCombo.setBounds(126, 53, 243, 27);
-		frame.getContentPane().add(guardCombo);
 
 
-		JTextArea txtGame = new JTextArea();
-		txtGame.setBounds(10, 94,  359, 375);
-		txtGame.setFont(new Font("Courier New", Font.PLAIN, 13));
-		txtGame.setRows(12);
-		txtGame.setColumns(12);
-		txtGame.setEditable(false);
-		frame.getContentPane().add(txtGame);
-
-		JButton exitButton = new JButton("Exit");
-		exitButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);	
-			}
-
-		});
-		exitButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		exitButton.setBounds(407, 422,  186, 41);
-		frame.getContentPane().add(exitButton);
-
-		JButton upButton = new JButton("UP");
-		upButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				game.update(Game.Direction.UP);
-				String s = game.toString();
-				txtGame.replaceRange(s, 0, s.length);
-			}
-
-		});
-		upButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		upButton.setBounds(437, 163, 103, 56);
-		upButton.setEnabled(false);
-		frame.getContentPane().add(upButton);
-
-		JButton leftButton = new JButton("LEFT");
-		leftButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				game.update(Game.Direction.LEFT);
-				String s = game.toString();
-				txtGame.replaceRange(s, 0, s.length);
-			}
-
-		});
-		leftButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		leftButton.setBounds(379, 240, 92, 56);
-		leftButton.setEnabled(false);
-		frame.getContentPane().add(leftButton);
-
-		JButton downButton = new JButton("DOWN");
-		downButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				game.update(Game.Direction.DOWN);
-				String s = game.toString();
-				txtGame.replaceRange(s, 0, s.length);
-			}
-
-		});
-		downButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		downButton.setBounds(437, 315, 103, 56);
-		downButton.setEnabled(false);
-		frame.getContentPane().add(downButton);
-
-		JButton rightButton = new JButton("RIGHT");
-		rightButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				game.update(Game.Direction.RIGHT);
-				String s = game.toString();
-				txtGame.replaceRange(s, 0, s.length);
-			}
-
-		});
-		rightButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		rightButton.setBounds(514, 240, 92, 56);
-		rightButton.setEnabled(false);
-		frame.getContentPane().add(rightButton);
-
-		lblGamestatus = new JLabel("GAME_STATUS");
-		lblGamestatus.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		lblGamestatus.setBounds(10, 479, 411, 22);
-		frame.getContentPane().add(lblGamestatus);
-
-		JButton startButton = new JButton("New Game");
-		startButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Integer ogreNumber;
-				String guardPersonality;
-				try {
-					ogreNumber = Integer.parseUnsignedInt(ogreTxt.getText());
-				}
-				catch(NumberFormatException ex) {
-					JOptionPane.showMessageDialog(frame, "Must introduce a positive integer number");
-					return;
-				}
-				guardPersonality = (String) guardCombo.getSelectedItem();
-
-				/* For future use	*/
-				upButton.setEnabled(true);
-				leftButton.setEnabled(true);
-				downButton.setEnabled(true);
-				rightButton.setEnabled(true);
-				
-				startButton.setEnabled(false);
-				/**/
-				return;
-				/* TODO
-				 * ChangeStatus
-				 * startGame(ogreNumber, guardPersonality);
-				 */
-			}
-
-		});
-		startButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		startButton.setBounds(407, 82,  186, 41);
-		frame.getContentPane().add(startButton);
-
-		ogreTxt = new JTextField();
-		ogreTxt.setBounds(126, 11,  67, 27);
-		frame.getContentPane().add(ogreTxt);
-		ogreTxt.setColumns(10);
 	}
 }
